3091313c006587b275e92ea3e9e4c8c01918cb5f
Melvin Chidiebere <chidiebere.johnbosco@gmail.com>
2021-10-13 17:40:08 +0100

    kendoui setup

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import "../styles/globals.css";
+import "@progress/kendo-theme-default/dist/all.css";
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,52 @@
-function Home() {
-  return <div>Helo from Home</div>;
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+// import CloudAssets from "../components/CloudAssets";
+import { Typography } from "@progress/kendo-react-common";
+
+export async function getStaticProps(context) {
+  const res = await fetch(
+    `https://res.cloudinary.com/kizmelvin/image/list/v1633911053/city.json`
+  );
+  const resData = await res.json();
+
+  if (!resData) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: { resData },
+
+    revalidate: 5,
+  };
 }
 
-export default Home;
+export default function Home({ resData }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>KendoUI Gallary </title>
+        <meta name="description" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="wrap k-align-items-center">
+        <Typography.h2 textAlign={"center"} fontWeight={"bold"}>
+          Awesome Gallary
+        </Typography.h2>
+        <Typography.p
+          textAlign={"center"}
+          themeColor={"inverse"}
+          fontWeight={"light"}
+        >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus quam
+          eos consequatur, <br /> rem ratione nesciunt quisquam dolorem,
+          repudiandae officia totam amet corporis illum <br /> minus fugit
+          incidunt magnam deserunt veniam dignissimos.
+        </Typography.p>
+        {/* <CloudAssets data={resData} /> */}
+      </main>
+    </div>
+  );
+}
